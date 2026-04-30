155438bbe63984610fd8bc6300699d30a01ffdec
pizzasdu83 <pizzasdu83@gmail.com>
2026-04-30 14:34:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -177,15 +177,21 @@
 				document.body.classList.add("color");
 				document.body.style.backgroundColor = color;
 				if (colorPicker) colorPicker.value = color; 
+				document.body.style.animationPlayState = "paused";
 			} else if (mode === "preset-gradient") {
 				document.body.classList.add("gradient");
+				document.body.style.animationPlayState = "paused";
 			} else if (mode === "r34") {
 				document.body.classList.add("r34");
+				document.body.style.animationPlayState = "paused";
 			} else if (mode === "preset-image") {
 				document.body.classList.add("image");
+				document.body.style.animationPlayState = "paused";
 			} else if (mode === "fallin") {
 				document.body.classList.add("fallin");
+				document.body.style.animationPlayState = "running";
 			} else if (mode === "persimage") {
+				document.body.style.animationPlayState = "paused";
 				const input = document.getElementById("avatar");
 
 				input.addEventListener("change", function () {
